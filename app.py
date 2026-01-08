320272de2f0939d88299afe8fd7da2aa5cdf0bfb
pharmaaofficial-glitch <pharmaa.official@gmail.com>
2026-01-08 13:38:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -48,4 +48,8 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run()
+    init_db()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
